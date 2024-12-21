7f504361cae3ce3deb7eb05ac1cdf948383dbd01
oqnix <benrudolph2005@gmail.com>
2024-12-22 00:44:13 +0100

    Add files via upload

---
+++ oqnix_givevehicle/config.lua
@@ -0,0 +1,7 @@
+Config = {}
+Config.Locale = 'en' -- de / en
+
+-- Add youre Admin Groups Here
+Config.AuthorizedRanks = {
+  'admin', -- youre Groups
+}
---
+++ oqnix_givevehicle/fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'adamant'
+game 'gta5'
+lua54 'yes'
+
+escrow_ignore {
+    'config.lua',
+    'locales/de.lua',
+    'locales/en.lua',
+}
+
+name "oqnix"
+description "Autos Vergeben per Command"
+version '1.0.0'
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    'server/main.lua',
+    'locales/de.lua',
+    'locales/en.lua',
+}
+
+client_scripts {
+    'client/main.lua',
+}
+
+shared_script 'config.lua'
+dependency '/assetpacks'
